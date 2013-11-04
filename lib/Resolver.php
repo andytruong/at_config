d2adda12dd2903ea8dba4813fb3a419152b76d2d
Andy Truong <thehongtt@gmail.com>
2013-11-04 17:59:55 +0700

    YAML file can now import other YAML files.

--- lib/Resolver.php
+++ lib/Resolver.php
@@ -52,6 +52,34 @@ class Resolver implements ResolverInterface {
    * @return mixed
    */
   public function fetchData() {
-    return at_config_read_yml($this->getPath());
+    $return = $this->fetchFile($this->getPath());
+    return $return;
+  }
+
+  private function fetchFile($path) {
+    $return = at_config_read_yml($path);
+
+    if (empty($return['imports'])) {
+      return $return;
+    }
+
+    $_return = array();
+    foreach ($return['imports'] as $i => $import) {
+      $resource = $import['resource'];
+      $resource = DRUPAL_ROOT . '/' . drupal_get_path('module', $this->config->getModule()) . '/config/' . $resource;
+      if ($data = $this->fetchFile($resource)) {
+        foreach ($data as $k => $v) {
+          $_return[$k] = !isset($_return[$k]) ? $v : array_merge($_return[$k], $v);
+        }
+      }
+    }
+
+    unset($return['imports']);
+
+    foreach ($return as $k => $v) {
+      $_return[$k] = !isset($_return[$k]) ? $v : array_merge($_return[$k], $v);
+    }
+
+    return $_return;
   }
 }
